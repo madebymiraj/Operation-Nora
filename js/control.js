2a57555ed6d6b6b1e3fde23670ea3e0ac7b839b0
qpsmiley <brianevans91@gmail.com>
2016-01-24 05:53:42 +0000

    major AJAX changes, set to AFS

--- js/control.js
+++ js/control.js
@@ -1,12 +1,35 @@
 //Created by EvansUX
 $("form#article-link").submit(function(){
     var data = {
-    'filename'          : $('input[name=filename]').val(),
+    'url'          : $('input[name=filename]').val(),
     };    
     
-    $.ajax({
+    var ajaxRequest = $.ajax({
       type: "POST",
-      url: "/php/scrape.php",
+      url: "http://web.njit.edu/~map68/nora/php/scrape.php",
       data: data
-    })
+    });
+
+
+ajaxRequest.done(function(data1){
+    console.log(data1);
+});
+ajaxRequest.fail(function(jqXHR, textStatus){
+    if (jqXHR.status === 0)
+    {
+        console.log('Not connect.n Verify Network.');
+    }
+    else if (jqXHR.status == 404)
+    {
+        console.log('Requested page not found. [404]');
+    }
+    else if (jqXHR.status == 500)
+    {
+        console.log('Internal Server Error [500].');
+    }
+    else
+    {
+        console.log('Uncaught Error.n' + jqXHR.responseText);
+    }
+});
 });
