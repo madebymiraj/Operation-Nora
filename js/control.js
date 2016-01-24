a7949bca6c400d122f6d3722e279990ee98ce5ed
qpsmiley <brianevans91@gmail.com>
2016-01-24 02:44:42 +0000

    wrote AJAX call for index.html page in control.js ... needs testing

--- js/control.js
+++ js/control.js
@@ -1,1 +1,12 @@
 //Created by EvansUX
+$("form#article-link").submit(function(){
+    var data = {
+    'filename'          : $('input[name=filename]').val(),
+    };    
+    
+    $.ajax({
+      type: "POST",
+      url: "/php/scrape.php",
+      data: data
+    })
+});
